cea33daa74857f8b74eefef3355462f85ec19c5f
Ren TengLong <rentenglong@163.com>
2018-11-27 22:14:38 +0800

    原型被列为旧项目被抛弃，现使用手写类库实现了覆盖式对话框交互和已修正的网格系统

--- OLD_VERSION_PROTOTYPE1/lib/js/bootstrap.bundle.js
+++ OLD_VERSION_PROTOTYPE1/lib/js/bootstrap.bundle.js
@@ -4,8 +4,8 @@
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
   */
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('jquery')) :
-  typeof define === 'function' && define.amd ? define(['exports', 'jquery'], factory) :
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('OLD_VERSION_PROTOTYPE1/lib/js/jQuery')) :
+  typeof define === 'function' && define.amd ? define(['exports', 'OLD_VERSION_PROTOTYPE1/lib/js/jQuery'], factory) :
   (factory((global.bootstrap = {}),global.jQuery));
 }(this, (function (exports,$) { 'use strict';
 
--- OLD_VERSION_PROTOTYPE1/lib/js/bootstrap.js
+++ OLD_VERSION_PROTOTYPE1/lib/js/bootstrap.js
@@ -4,8 +4,8 @@
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
   */
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('jquery'), require('popper.js')) :
-  typeof define === 'function' && define.amd ? define(['exports', 'jquery', 'popper.js'], factory) :
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('OLD_VERSION_PROTOTYPE1/lib/js/jQuery'), require('popper.js')) :
+  typeof define === 'function' && define.amd ? define(['exports', 'OLD_VERSION_PROTOTYPE1/lib/js/jQuery', 'popper.js'], factory) :
   (factory((global.bootstrap = {}),global.jQuery,global.Popper));
 }(this, (function (exports,$,Popper) { 'use strict';
 
